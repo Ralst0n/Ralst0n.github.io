60dd47e27315548c6f0deea5e1377587abf154ed
Ralst0n <pghRalston@gmail.com>
2017-11-09 18:24:54 -0500

    remove commented out code, add comments explaining functions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var old_p;
 $("document").ready(function(){
   $(".project > a > img").hover(
+    // change the paragraph below the image to view live when the image is hovered over
     function(){
        old_p = $(this).parent().parent().children("div").children("a").children("p").html();
       $(this).parent().parent().children("div").children("a").children("p").html("<p>View Live</p>");
@@ -11,6 +12,7 @@ $("document").ready(function(){
   );
 
   $(".project-description").hover(
+    // change the paragraph below the image to view code when the paragraph block is hovered over
     function(){
       old_p = $(this).children("a").children("p").html();
       $(this).children("a").children("p").html("<p>View Code</p>");
@@ -20,16 +22,5 @@ $("document").ready(function(){
     }
   )
 
-  // $(".project").hover(
-  //   function(){
-  //      old_p = $(this).children("div").children("p").html()
-  //     $(this).children("div").children("p").html("<p>View Live</p>");
-  //   },
-  //   function(){
-  //     $(this).children("div").children("p").html(old_p);
-  //   }
-  // );
-
-  $(".")
 
 })
